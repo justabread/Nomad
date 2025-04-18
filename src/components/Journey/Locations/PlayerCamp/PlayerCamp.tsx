9e0ef26f6ca4b653d17dff13f13409a8368a1c4f
justabread <pappjanino@gmail.com>
2025-04-18 18:35:47 +0200

    camp healing added

--- src/components/Journey/Locations/PlayerCamp/PlayerCamp.tsx
+++ src/components/Journey/Locations/PlayerCamp/PlayerCamp.tsx
@@ -7,6 +7,10 @@ const PlayerCamp = () => {
   const { player, setPlayerHealth, setPlayerFoodItems, setPlayerLocation } =
     useContext(GameMasterContext);
 
+  const HP_LOSS_NO_FOOD = 25;
+  const HP_LOSS_SOME_FOOD = 10;
+  const FOOD_LOSS = 2;
+
   return (
     <div className="UI-element">
       <h1>You have set up camp for the night.</h1>
@@ -15,7 +19,8 @@ const PlayerCamp = () => {
           return (
             <p>
               You do not have enough food to put anything on the campfire. As so
-              many times before, you are going to starve tonight. (-25 HP)
+              many times before, you are going to starve tonight. (-
+              {HP_LOSS_NO_FOOD} HP)
             </p>
           );
         } else if (player.foodItems === 1) {
@@ -23,14 +28,14 @@ const PlayerCamp = () => {
             <p>
               You managed to scrape together enough food that you will at least
               be able to sleep. &quot;Tomorrow will be better&quot; - You tell
-              yourself. (-10 HP)
+              yourself. (-{HP_LOSS_SOME_FOOD} HP)
             </p>
           );
         } else {
           return (
             <p>
-              You cook some food on your campfire (-2 Food). The food warms you
-              up and eases your weary mind.
+              You cook some food on your campfire (-{FOOD_LOSS} Food). The food
+              warms you up and eases your weary mind (Health Fully Restored).
             </p>
           );
         }
@@ -41,12 +46,14 @@ const PlayerCamp = () => {
           <button
             onClick={() => {
               if (player.foodItems <= 0) {
-                setPlayerHealth(player.health - 25);
+                setPlayerHealth(player.health - HP_LOSS_NO_FOOD);
               } else if (player.foodItems <= 2) {
-                setPlayerHealth(player.health - 10);
+                setPlayerHealth(player.health - HP_LOSS_SOME_FOOD);
+              } else {
+                setPlayerHealth(100);
               }
 
-              setPlayerFoodItems(player.foodItems - 2);
+              setPlayerFoodItems(player.foodItems - FOOD_LOSS);
               setPlayerLocation(element.name);
             }}
             key={i}
